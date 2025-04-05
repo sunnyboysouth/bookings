7f54b6ddd5e0782456e50463cca1763bb6662747
sunnyboysouth <dsundareswaran@gmail.com>
2025-04-05 15:43:10 +0100

    adding html-sources files

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This is the repository for a sample golang project
 
 - Built in Go v1.23.3
-- Uses the [chi package] (https://github.com/go-chi/chi/v5) for router and middleware
-- uses [alex edwards for SEC] (https://github.com/alexedwards/scs/v2) session management
-- uses [nosurf] (https://github.com/justinas/nosurf)
+- Uses the [chi package](https://github.com/go-chi/chi/v5) for router and middleware
+- uses [alex edwards for SEC](https://github.com/alexedwards/scs/v2) session management
+- uses [nosurf](https://github.com/justinas/nosurf)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,10 +3,10 @@ package main
 import (
 	"net/http"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/dsundar/bookings/pkg/config"
 	"github.com/dsundar/bookings/pkg/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -19,6 +19,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
